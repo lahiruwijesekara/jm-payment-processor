82847b0628eceeb2dda6577782e2f772e695ec86
Lahiru Wijesekara <lahirusandaruwan@gmail.com>
2021-03-21 11:38:14 +0530

    Initialization
    Initial commit

---
+++ src/main/java/com/javamastermind/payment/JmPaymentProcessApplication.java
@@ -0,0 +1,13 @@
+package com.javamastermind.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmPaymentProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmPaymentProcessApplication.class, args);
+	}
+
+}
